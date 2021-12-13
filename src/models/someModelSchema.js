ba5c914a42b0fca2a93b5e0c19c8657910992e8e
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-12-12 21:45:29 -0300

    adding a model example from schema

---
+++ src/models/someModelSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Define schema
+const Schema = mongoose.Schema;
+
+const SomeModelSchema = new Schema ({
+  a_string: String,
+  a_date: Date
+});
+
+// Compile model from schema
+const SomeModel = mongoose.model('SomeModel', SomeModelSchema);
