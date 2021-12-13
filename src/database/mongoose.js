17a3bf4fa2cc071b5f92258359044d9cb1f43043
Vitor Figueiredo <vitorfigueiredo855@gmail.com>
2021-12-12 21:48:53 -0300

    remove database uri

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Set up default mongoose connection
-const mongoDB = 'mongodb+srv://vitor:vitor123@cluster0.hgdbc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const mongoDB = process.env.DATABASE_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true});
 
 // Get the default connection
